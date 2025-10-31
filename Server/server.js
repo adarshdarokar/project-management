c9887a07fa60d2fb52968fb1d5bf33a7dcc2cd65
ADARSH DAROKAR <adarshdarokar55@gmail.com>
2025-10-31 14:23:04 +0530

    first commit

---
+++ Server/server.js
@@ -0,0 +1,21 @@
+import express from "express"
+import "dotenv/config"
+import cors from "cors"
+import { clerkMiddleware } from '@clerk/express'
+import { inngest, functions } from "./inngest/index.js"
+import { serve } from "inngest/express";
+
+const app = express()
+
+
+app.use(clerkMiddleware())
+app.use (express.json());
+app.use(cors())
+
+app.get('/' ,(req, res)=> res.send('server is live'))
+app.use("/api/inngest", serve({ client: inngest, functions }));
+
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, ()=> console.log(`server is running on port ${ PORT}`))

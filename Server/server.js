62810b35c0b2e60b98aa4afe12014db25bb8eb55
ADARSH DAROKAR <adarshdarokar55@gmail.com>
2025-11-01 00:53:37 +0530

    prisma-3

--- Server/server.js
+++ Server/server.js
@@ -6,7 +6,6 @@ import { inngest, functions } from "./inngest/index.js"
 import { serve } from "inngest/express";
 
 const app = express()
-``
 
 app.use(clerkMiddleware())
 app.use (express.json());
